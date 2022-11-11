f2c54ceb5df235b263d53fca755f9bd008451685
S <siddtheone@gmail.com>
2022-11-11 21:07:40 +0530

    Updating header component

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <>
-      <h1>Hiii</h1>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
